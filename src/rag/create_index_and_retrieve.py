6d6becf49e0acc5bf07c4c0b76c904f32afaad1c
Silvana Hartmann <codeberg@silvanahartmann.de>
2025-03-14 15:18:26 +0100

    add lemmatizer/tokenizer to bm25retriever

--- src/rag/create_index_and_retrieve.py
+++ src/rag/create_index_and_retrieve.py
@@ -1,11 +1,16 @@
+from assertpy import assert_that
 from langchain_core.documents import Document
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_mistralai import MistralAIEmbeddings
+from langchain_text_splitters import spacy
 from transformers import AutoModel, AutoTokenizer
 from langchain_community.retrievers import BM25Retriever
 from langchain.retrievers import EnsembleRetriever
 from langchain_community.vectorstores import FAISS
-
+from nltk.tokenize import word_tokenize
+import spacy
+import nltk
+#nltk.download('punkt_tab')
 
 
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
@@ -13,14 +18,21 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
 
 
+def lemmatized(text: str):
+    nlp = spacy.load("de_core_news_sm")
+    return [str(word.lemma_) for word in nlp(text)]
+
 def get_bm25_retriever_from_text(texts: [str], source_id: int, num_results: int) -> BM25Retriever:
     # TODO additional metadata?
-    retriever = BM25Retriever.from_texts(texts, metadatas=[{"source": source_id}] * len(texts))
+    retriever = BM25Retriever.from_texts(texts, metadatas=[{"source": source_id}] * len(texts), preprocess_func=lemmatized)
+#    retriever = BM25Retriever.from_texts(texts, metadatas=[{"source": source_id}] * len(texts), preprocess_func=word_tokenize(language='german'))
     retriever.k = num_results
     return retriever
 
 def get_bm25_retriever_from_documents(documents: [Document], source_id: int, num_results: int) -> BM25Retriever:
-    retriever = BM25Retriever.from_documents(documents)#, metadatas=[{"source": source_id}] * len(documents))
+    retriever = BM25Retriever.from_documents(documents, preprocess_func=lemmatized)
+    #retriever = BM25Retriever.from_documents(documents, preprocess_func=word_tokenize)
+    #retriever = BM25Retriever.from_documents(documents)#, metadatas=[{"source": source_id}] * len(documents))
     retriever.k = num_results
     return retriever
 
@@ -32,18 +44,16 @@ def get_faiss_vectorstore_retriever_from_text(texts: [str], source_id: int, num_
 
 def get_faiss_vectorstore_retriever_from_documents(documents: [Document], source_id: int, num_results: int) -> VectorStoreRetriever:
     embedding = MistralAIEmbeddings(model="mistral-embed")
-    vectorstore = FAISS.from_documents(documents, embedding) #, embedding, metadatas=[{"source": source_id}] * len(documents))
+    vectorstore = FAISS.from_documents(documents, embedding) #, embedding, metadatas=[{"source": source_id}] * len(documents)) ## TODO add documents here
     faiss_retriever = vectorstore.as_retriever(search_kwargs={"k": num_results})
     return faiss_retriever
 
 def get_ensemble_retriever_from_texts(texts: [str], source_id: int, num_results: int) -> EnsembleRetriever:
-    # TODO restrict results NO?
     return EnsembleRetriever(retrievers=[get_bm25_retriever_from_text(texts, source_id, num_results),
                                          get_faiss_vectorstore_retriever_from_text(texts, source_id, num_results)],
                              weights=[0.5,0.5])
 
 def get_ensemble_retriever_from_documents(documents: [Document], source_id: int, num_results: int) -> EnsembleRetriever:
-    # TODO restrict results NO?
     return EnsembleRetriever(retrievers=[get_bm25_retriever_from_documents(documents, source_id, num_results),
                                          get_faiss_vectorstore_retriever_from_documents(documents, source_id, num_results)],
                              weights=[0.5,0.5])
